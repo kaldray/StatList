1b987dce3cb0e6142bf9a750bc15ae00d3f3fb1f
kaldray <jordray@protonmail.com>
2022-09-25 18:26:52 +0200

    style: add padding to Layout

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Navigation } from "./Navigation";
 
+import styles from "@styles/Components/Layout.module.scss";
+
 type LayoutProps = {
   children: JSX.Element;
 };
 
 export const Layout = ({ children }: LayoutProps) => {
+  const { container } = styles;
   return (
     <>
       <Navigation />
-      <section>{children}</section>
+      <section className={container}>{children}</section>
     </>
   );
 };
