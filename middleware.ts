7752a4535b39ccab5454ef40a69e96b9268fe3d8
kaldray <jordray@protonmail.com>
2022-12-21 09:35:57 +0100

    fix: middleware redirection

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export async function middleware(req: NextRequest): Promise<NextResponse | undefined> {
+export async function middleware(req: NextRequest): Promise<NextResponse> {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   if (token !== null && req.nextUrl.pathname.startsWith("/" + token.provider)) {
-    NextResponse.next();
-  } else {
-    return NextResponse.redirect(new URL("/", req.url));
+    return NextResponse.next();
   }
   return NextResponse.redirect(new URL("/", req.url));
 }
