1d0a928ef2beb635021c8aee5fc3dd959b6b6f3e
kaldray <jordray@protonmail.com>
2022-12-25 16:40:26 +0100

    fix: remove Layout from index pages

--- pages/deezer/index.tsx
+++ pages/deezer/index.tsx
@@ -3,8 +3,6 @@ import { getToken } from "next-auth/jwt";
 
 import type { NextPage, GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 
-import { Layout } from "@components/index";
-
 import styles from "@styles/Pages/home.module.scss";
 
 import { getServerSideUserInfo } from "types/next";
@@ -21,22 +19,21 @@ const Home: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = (
         <meta name="description" content="Application web qui permet de consulter vos stats spotify." />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
       </Head>
-      <Layout>
-        <section className={presentation__container}>
-          <h1>Bienvenue sur StatList {token.name}</h1>
-          <p>
-            Vous êtes connectez avec votre compte <strong>{token.provider}</strong>
-          </p>
-          <p>
-            Grâce à notre outil, vous pouvez découvrir <strong> vos artistes les plus écoutés</strong> et{" "}
-            <strong>vos titres les plus populaires</strong>, en temps réel.
-          </p>
-          <p>
-            Pour accéder à vos statistiques, rendez-vous sur les pages de <span>meilleur artiste</span> ou{" "}
-            <span>de meilleure</span> chanson.
-          </p>
-        </section>
-      </Layout>
+
+      <section className={presentation__container}>
+        <h1>Bienvenue sur StatList {token.name}</h1>
+        <p>
+          Vous êtes connectez avec votre compte <strong>{token.provider}</strong>
+        </p>
+        <p>
+          Grâce à notre outil, vous pouvez découvrir <strong> vos artistes les plus écoutés</strong> et{" "}
+          <strong>vos titres les plus populaires</strong>, en temps réel.
+        </p>
+        <p>
+          Pour accéder à vos statistiques, rendez-vous sur les pages de <span>meilleur artiste</span> ou{" "}
+          <span>de meilleure</span> chanson.
+        </p>
+      </section>
     </>
   );
 };
