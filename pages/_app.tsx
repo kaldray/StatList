8cb346a505e63baff241acfe29a6fbf8817bc5e5
Jordray Kalenga <78658124+kaldray@users.noreply.github.com>
2022-12-30 20:35:28 +0100

    Next Font Support (#14)
    * feat: add next font support
    
    * fix: remove Layout wrapper
    
    * fix: remove Layout from term
    
    * fix: show hamburger on mobile view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Layout } from "@components/index";
+import { Lato } from "@next/font/google";
 import "@styles/Global/reset.scss";
+
+const lato = Lato({
+  weight: ["400", "700", "900", "300"],
+  style: ["italic", "normal"],
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <SessionProvider session={pageProps.session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <main>
+        <Layout font={lato}>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
     </SessionProvider>
   );
 }
