46cb15eab9249ccac45dddb62abb02768254e338
kaldray <jordray@protonmail.com>
2025-06-14 16:14:42 +0200

    chore: react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,26 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import { fileURLToPath } from "node:url";
 
+const ReactCompilerConfig = {
+  target: "19",
+};
+import babel from "vite-plugin-babel";
+
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [reactRouter()],
+  plugins: [
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"],
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+    ,
+    reactRouter(),
+  ],
   resolve: {
     alias: {
       "@styles": fileURLToPath(new URL("./scss", import.meta.url)),
