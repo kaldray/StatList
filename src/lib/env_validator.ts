ff24f9c850c0170c88225441042fd0cdd1e622d2
kaldray <jordray@protonmail.com>
2025-06-15 17:41:12 +0200

    test prefix VITE

--- src/lib/env_validator.ts
+++ src/lib/env_validator.ts
@@ -3,7 +3,7 @@ import { z } from "zod/v4";
 // Define the schema as an object with all of the env
 // variables and their types
 const envSchema = z.object({
-  NEXTAUTH_URL: z.string(),
+  VITE_NEXTAUTH_URL: z.string(),
   SPOTIFY_CLIENT_ID: z.string(),
   SPOTIFY_CLIENT_SECRET: z.string(),
   DEEZER_CLIENT_ID: z.string(),
