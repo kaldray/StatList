7fbb53330d6e947ef816d86520c02d6f1a70208c
kaldray <jordray@protonmail.com>
2025-06-15 14:00:45 +0200

    css

--- src/root.tsx
+++ src/root.tsx
@@ -18,7 +18,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>My App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <title>Statlist</title>
         <Meta />
         <Links />
       </head>
