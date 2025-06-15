5e43f40b698d54f852aacf61599b3549a9c88c45
kaldray <jordray@protonmail.com>
2025-06-15 18:03:28 +0200

    test with .server

--- src/routes/auth/autorization.tsx
+++ src/routes/auth/autorization.tsx
@@ -1,18 +1,25 @@
 import { redirect } from "react-router";
 
 import type { Route } from "./+types/autorization";
-import { SpotifyApi } from "@src/lib/auth";
+import { SpotifyApi } from "@src/lib/auth.server";
 import { getSession, commitSession } from "@src/sessions.server";
 import { assertIsString, generateState } from "@src/utils";
-import { env } from "@src/lib/env_validator";
+import { env } from "@src/lib/env_validator.server";
 
 export async function loader({ request, params }: Route.LoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
   const state = generateState();
   if (params.provider === "spotify") {
-    const SPOTIFY_API = new SpotifyApi(env.SPOTIFY_CLIENT_ID, env.SPOTIFY_CLIENT_SECRET);
-    session.set("state", state);
-    assertIsString(state);
-    return redirect(SPOTIFY_API.get_authorization(state), { headers: { "Set-Cookie": await commitSession(session) } });
+    if (!env.success) {
+      console.error("Spotify API is not configured");
+      console.error(env.data);
+    } else {
+      const SPOTIFY_API = new SpotifyApi(env.data.SPOTIFY_CLIENT_ID, env.data.SPOTIFY_CLIENT_SECRET);
+      session.set("state", state);
+      assertIsString(state);
+      return redirect(SPOTIFY_API.get_authorization(state), {
+        headers: { "Set-Cookie": await commitSession(session) },
+      });
+    }
   }
 }
