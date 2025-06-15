94ef06d453246333c966ba75df5fa4d1f17dfc4a
kaldray <jordray@protonmail.com>
2025-06-15 18:07:38 +0200

    fix condition

--- src/routes/spotify/index.tsx
+++ src/routes/spotify/index.tsx
@@ -9,7 +9,7 @@ import type { Route } from "./+types/index";
 import styles from "@styles/Pages/home.module.scss";
 import { data, redirect } from "react-router";
 import { assertIsDefined, assertIsString } from "@src/utils";
-import { env } from "@src/lib/env_validator";
+import { env } from "@src/lib/env_validator.server";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
@@ -27,28 +27,33 @@ export async function loader({ request }: Route.LoaderArgs) {
   const FIVE_MINUTES_IN_MS = 5 * 60 * 1000;
 
   if (expired_at - now <= FIVE_MINUTES_IN_MS) {
-    const SpotifyApi = (await import("@src/lib/auth")).SpotifyApi;
-    const spotifyApi = new SpotifyApi(env.SPOTIFY_CLIENT_ID, env.SPOTIFY_CLIENT_SECRET);
-    const response = await spotifyApi.refresh_access_token(user_session.refresh_token);
-    session.set("statlist_user", {
-      display_name: user_session.display_name,
-      access_token: response.access_token,
-      expires_in: new Date(Date.now() + response.expires_in * 1000),
-      refresh_token: user_session.refresh_token ?? response.refresh_token,
-      provider: "spotify",
-    });
+    const SpotifyApi = (await import("@src/lib/auth.server")).SpotifyApi;
+    if (!env.success) {
+      console.error("Spotify API key is not defined");
+      console.log(env.data);
+    } else {
+      const spotifyApi = new SpotifyApi(env.data.SPOTIFY_CLIENT_ID, env.data.SPOTIFY_CLIENT_SECRET);
+      const response = await spotifyApi.refresh_access_token(user_session.refresh_token);
+      session.set("statlist_user", {
+        display_name: user_session.display_name,
+        access_token: response.access_token,
+        expires_in: new Date(Date.now() + response.expires_in * 1000),
+        refresh_token: user_session.refresh_token ?? response.refresh_token,
+        provider: "spotify",
+      });
 
-    return data(
-      { name: user_session.display_name, provider: user_session.provider },
-      {
-        headers: {
-          "Set-Cookie": await commitSession(session, {
-            expires: new Date(Date.now() + response.expires_in * 1000),
-            maxAge: response.expires_in,
-          }),
+      return data(
+        { name: user_session.display_name, provider: user_session.provider },
+        {
+          headers: {
+            "Set-Cookie": await commitSession(session, {
+              expires: new Date(Date.now() + response.expires_in * 1000),
+              maxAge: response.expires_in,
+            }),
+          },
         },
-      },
-    );
+      );
+    }
   }
 
   return data({ name: user_session.display_name, provider: user_session.provider });
