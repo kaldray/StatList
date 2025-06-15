80be0b1d5b42a179e576cac2ee7c557919cf5598
kaldray <jordray@protonmail.com>
2025-06-15 13:32:35 +0200

    rename and clean

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,27 @@
 // @ts-check
-import { defineConfig } from "eslint/config";
 import react from "eslint-plugin-react";
-import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 import globals from "globals";
 
-// export default defineConfig(
-//   reactHooks.configs["recommended-latest"],
-//   tseslint.configs.recommended,
-//   react.configs["recommended"],
-// );
-
 export default tseslint.config(
   {
+    ignores: [
+      "**/*.d.ts",
+      "**/*.test.ts",
+      "**/*.test.tsx",
+      "**/*.config.{mjs,ts}",
+      "**/*.json",
+      "node_modules",
+      ".react-router",
+      ".*",
+    ],
     files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
     plugins: {
       react,
     },
     languageOptions: {
+      parser: tseslint.parser,
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
@@ -30,12 +33,19 @@ export default tseslint.config(
     },
     settings: {
       react: {
-        version: "detect",
+        version: "19",
       },
     },
   },
+  ...tseslint.configs.recommended,
   react.configs.flat.recommended,
   react.configs.flat["jsx-runtime"],
   reactHooks.configs["recommended-latest"],
-  tseslint.configs.recommended,
+  {
+    files: [".react-router/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
 );
