398936eb6a708d0670135b3bdfe790870268eb1e
kaldray <jordray@protonmail.com>
2025-06-23 13:27:18 +0200

    fix:remove unused

--- src/lib/env_validator.server.ts
+++ src/lib/env_validator.server.ts
@@ -1,10 +1,7 @@
 import { z } from "zod/v4";
 
 export const envSchema = z.object({
-  NEXTAUTH_URL: z.string(),
   SPOTIFY_CLIENT_ID: z.string(),
   SPOTIFY_CLIENT_SECRET: z.string(),
-  DEEZER_CLIENT_ID: z.string(),
-  DEEZER_CLIENT_SECRET: z.string(),
   NEXTAUTH_SECRET: z.string(),
 });
