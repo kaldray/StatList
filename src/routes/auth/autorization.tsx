e44840cd6799bb085035363506c69f0a95ef6a86
kaldray <jordray@protonmail.com>
2025-06-23 13:09:17 +0200

    last one

--- src/routes/auth/autorization.tsx
+++ src/routes/auth/autorization.tsx
@@ -3,13 +3,11 @@ import { redirect } from "react-router";
 import type { Route } from "./+types/autorization";
 import { getSession, commitSession } from "@src/sessions.server";
 import { assertIsString, generateState } from "@src/utils";
-import { validateEnv } from "@src/lib/env_validator.server";
 
 export async function loader({ request, params }: Route.LoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
   const state = generateState();
   if (params.provider === "spotify") {
-    const env = validateEnv();
     const SpotifyApi = (await import("@src/lib/auth.server")).SpotifyApi;
     const spotifyApi = new SpotifyApi(process.env.SPOTIFY_CLIENT_ID, process.env.SPOTIFY_CLIENT_SECRET);
     session.set("state", state);
